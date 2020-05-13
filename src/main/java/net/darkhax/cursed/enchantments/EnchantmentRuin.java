3e8ba2b6ef329ef296d90c1d343d8078e7525268
Tyler Hancock <darklime@live.ca>
2020-05-13 04:43:54 -0600

    Added the curse of ruin.

---
+++ src/main/java/net/darkhax/cursed/enchantments/EnchantmentRuin.java
@@ -0,0 +1,28 @@
+package net.darkhax.cursed.enchantments;
+
+import net.darkhax.bookshelf.enchantment.EnchantmentCurse;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.EnchantmentType;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.AnvilUpdateEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+
+public class EnchantmentRuin extends EnchantmentCurse {
+    
+    public EnchantmentRuin() {
+        
+        super(EnchantmentType.BREAKABLE, EquipmentSlotType.values());
+        MinecraftForge.EVENT_BUS.addListener(EventPriority.LOWEST, this::onAnvilUpdate);
+    }
+    
+    private void onAnvilUpdate (AnvilUpdateEvent event) {
+        
+        if (EnchantmentHelper.getEnchantmentLevel(this, event.getLeft()) > 0 || EnchantmentHelper.getEnchantmentLevel(this, event.getRight()) > 0) {
+            
+            event.setOutput(ItemStack.EMPTY);
+            event.setCanceled(true);
+        }
+    }
+}
